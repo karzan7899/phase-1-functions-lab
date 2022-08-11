67cb96c09923218fa56f4b5e6b9dfc1a9af8ecea
karzan <karzanluqman99@gmail.com>
2022-08-11 15:37:37 +0300

    partial

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    let y = (x - 42)
+    y = Math.abs(y);
+    return y;
+}
+
+function distanceFromHqInFeet(y) {
+    return (distanceFromHqInBlocks(y) * 264);
+}
+function distanceTravelledInFeet(s, d) {
+    return Math.abs(d - s) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    let y = distanceTravelledInFeet(start, destination)
+    if (y <= 400) {
+
+    }
+}
