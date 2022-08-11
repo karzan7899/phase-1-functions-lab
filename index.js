49ba18a4cc140056f8d26f2a7143b2d49af7de3d
karzan <karzanluqman99@gmail.com>
2022-08-11 15:44:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function distanceTravelledInFeet(s, d) {
 function calculatesFarePrice(start, destination) {
     let y = distanceTravelledInFeet(start, destination)
     if (y <= 400) {
-
+        return 0;
+    } else if (y >= 400 && y <= 2000) {
+        return (y - 400) * 0.02
     }
+    else if (y > 2000 && y <= 2500) {
+        return 25;
+    }
+    else if (y > 2500) { return 'cannot travel that far'; }
 }
